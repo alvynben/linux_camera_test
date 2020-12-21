f095159d5a4da4d676aa3035506d187269cfdee2
alvynben <71116618+alvynben@users.noreply.github.com>
2020-12-21 15:03:59 +0800

    version of opencv for csi cameras

---
+++ OpenCV-2.py
@@ -0,0 +1,13 @@
+import cv2
+print(cv2.__version__)
+
+camSet='nvarguscamerasrc sensor-id=0 ! video/x-raw(memory:NVMM), width=3264, height=2464, framerate=21/1, format=NV12 ! nvvidconv flip-method=2 ! video/x-raw, width=800, height=600, format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink '
+
+while True:
+    _, frame = cam.read()
+    cv2.imshow('myCam',frame)
+    cv2.moveWindow('myCam',0,0)
+    if cv2.waitKey(1)==ord('q'):
+        break
+cam.release()
+cv2.destroyAllWindows()
